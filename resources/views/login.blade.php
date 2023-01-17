751a93ee36a35f4c33ae024e4381976c67d263bc
jcmvdb <mvdb.jeroen@gmail.com>
2023-01-17 11:51:14 +0100

    login page style
    Added the styling for the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,10 +6,14 @@
 
 @section("content")
     <div id="wrapperlogin">
-    <form>
-        <input type="text" name="email" placeholder="e-mailadres">
-        <input type="password" name="password" placeholder="wachtwoord">
-        <input type="submit" name="submit" value="submit">
-    </form>
+        <div class="form">
+            <img src="/assets/img/brantweerj.png" alt="Brandweer" class="image">
+            <form>
+                <h1>Log in</h1>
+                <input type="email" name="email" placeholder="e-mailadres">
+                <input type="password" name="password" placeholder="wachtwoord">
+                <input type="submit" name="submit" value="submit">
+            </form>
+        </div>
     </div>
 @endsection
