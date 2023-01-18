3c8a29632e895ee75e519830485f5ce17075eca0
jcmvdb <mvdb.jeroen@gmail.com>
2023-01-18 13:37:19 +0100

    loigin page style responsive

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,15 +5,21 @@
 @endsection
 
 @section("content")
-    <div id="wrapperlogin">
-        <div class="form">
-            <img src="/assets/img/brantweerj.png" alt="Brandweer" class="image">
-            <form>
-                <h1>Log in</h1>
-                <input type="email" name="email" placeholder="e-mailadres">
-                <input type="password" name="password" placeholder="wachtwoord">
-                <input type="submit" name="submit" value="submit">
-            </form>
+    <div id="loginScreenWrapper">
+        <div class="redblur">
+            <div class="login">
+                <div class="logo">
+                    <img src="assets/img/brantweerj.png" alt="brandweer">
+                </div>
+                <div class="loginForm">
+                    <h1>Log in</h1>
+                    <form>
+                        <input type="email" name="email" placeholder="john@doe.com">
+                        <input type="password" name="password" placeholder="password">
+                        <input type="submit" name="submit" value="Log in">
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
