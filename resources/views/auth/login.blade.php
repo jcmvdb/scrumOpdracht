7b3093e4da55e38d129ee831b63d44520605b5ee
jcmvdb <mvdb.jeroen@gmail.com>
2023-01-24 13:42:01 +0100

    logo change loginpage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
         <div class="redblur">
             <div class="login">
                 <div class="logo">
-                    <img src="assets/img/brantweerj.png" alt="brandweer">
+                    <img src="assets/img/loginLogo.png" alt="brandweer">
                 </div>
                 <div class="loginForm">
                     <h1>Log in</h1>
