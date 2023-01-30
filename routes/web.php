265122e51fc20cce020f2c7a4dd56693d94f1b92
jcmvdb <mvdb.jeroen@gmail.com>
2023-01-30 14:49:15 +0100

    Routing login fixed

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -27,9 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-route::get('/logintest', function () {
-    return view ('logintest');
-}); 
 
 require __DIR__.'/auth.php';
