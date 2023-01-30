022693f00398050b17219a1e33ec9155a73d21e1
jcmvdb <mvdb.jeroen@gmail.com>
2023-01-30 14:49:48 +0100

    login page styling
    changed the border-radius for the login page and added the layout to the dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,12 @@
+@extends("layout")
+
+@section("title")
+    dashboard
+@endsection
+
+
+@section("content")
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -15,3 +24,5 @@
         </div>
     </div>
 </x-app-layout>
+dashboard
+@endsection
