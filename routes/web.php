93df22de47e24136ab706ed3be5f3f790f913592
jcmvdb <mvdb.jeroen@gmail.com>
2023-02-21 08:43:52 +0100

    updated the web
    added some pages for the vehicles and people

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Report;
+use App\Http\Controllers\ReportController;
+use App\Models\FormUsers;
+use App\Http\Controllers\FormUsersController;
+use App\Models\Vehicle;
+use App\Http\Controllers\VehicleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,16 +20,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        "users" => FormUsers::all(),
+        "vehicles" => Vehicle::all(),
+    ]);
+//    [ReportController::class, "index"];
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::post("dashboard", [ReportController::class, "reportsave"]);
 
 Route::get("/log", function () {
-    return view("log");
+    return view("log",
+        [
+            "Reportlog" => Report::all()
+        ]);
 })->middleware(['auth', 'verified'])->name("log");
 
 Route::middleware('auth')->group(function () {
@@ -32,4 +42,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get("/manschappen", function () {
+    return view("users", [
+        "users" => FormUsers::all(),
+    ]);
+});
+Route::post('/manschappen', [FormUsersController::class, "persoonToevoegen"]);
+Route::post('/updateperson', [FormUsersController::class, "persoonUpdaten"]);
+Route::get('/updateperson', [FormUsersController::class, "index"]);
+
+Route::get("/voertuigen", function () {
+    return view("vehicle", [
+        "vehicles" => Vehicle::all(),
+    ]);
+});
+Route::post('/voertuigen', [VehicleController::class, "addVehicle"]);
+
+
+require __DIR__ . '/auth.php';
